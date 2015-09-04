29a8a5294ce92b8526509e65184682868551b94d
timmyreilly <timreilly@live.com>
2015-09-04 16:25:26 -0700

    working

--- app/views.py
+++ app/views.py
@@ -7,31 +7,7 @@ from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
 
-def home(request):
-    """Renders the home page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Microsoft Code Challege',
-            'year':datetime.now().year,
-        })
-    )
 
-def anotherpage(request):
-    assert isinstance(request, HttpRequest)
-    return render(
-        request, 
-        'app/anotherpage.html',
-        context_instance = RequestContext(request, 
-            {
-                'name': 'Timothy',
-                'twitter': '@timmyreilly',
-                'year': datetime.now().year,
-            })
-    )
     
 def base(request):
     assert isinstance(request, HttpRequest)
@@ -59,6 +35,33 @@ def base(request):
                 'twitter': '@timmyreilly',
                 'number': '503-314-3771',
                 
+                'year': datetime.now().year,
+            })
+    )
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Microsoft Code Challege',
+            'year':datetime.now().year,
+        })
+    )
+
+def anotherpage(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request, 
+        'app/anotherpage.html',
+        context_instance = RequestContext(request, 
+            {
+                'name': 'Timothy',
+                'twitter': '@timmyreilly',
                 'year': datetime.now().year,
             })
     )
